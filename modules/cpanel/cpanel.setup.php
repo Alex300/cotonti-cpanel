01f3f751531494dce15abf3b1a60b7650234fb6e
Alexey Kalnov <a.kalnov@app-smart.ru>
2021-09-14 12:03:38 +0300

    Update info

--- modules/cpanel/cpanel.setup.php
+++ modules/cpanel/cpanel.setup.php
@@ -6,8 +6,8 @@ Name=Admin Control Panel
 Description=Admin Control Panel module for Cpanel admin theme
 Version=0.0.4
 Date=2017-03-16
-Author=Kalnov Alexey    (kalnovalexey@yandex.ru)
-Copyright=(с) 2014-2017 Portal30 Studio http://portal30.ru
+Author=Kalnov Alexey    <kalnovalexey@yandex.ru>
+Copyright=Copyright (c) 2014-2021 Lily Software https://lily-software.com (ex. Portal30 Studio)
 Auth_guests=
 Lock_guests=RW12345A
 Auth_members=
@@ -24,6 +24,6 @@ Lock_members=
  * @package Cotonti
  * @subpackage  Admin
  * @author Kalnov Alexey <kalnovalexey@yandex.ru>
- * @copyright © Portal30 2014-2017 Studio http://portal30.ru
+ * @copyright © 2014-2021 Lily Software https://lily-software.com (ex. Portal30 Studio)
  */
 defined('COT_CODE') or die('Wrong URL');
